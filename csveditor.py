d4160f87a1988f93719745b715bcdcf61652bef5
Kelton114 <keltonylli@gmail.com>
2024-04-20 10:53:05 +0800

    save

--- csveditor.py
+++ csveditor.py
@@ -13,13 +13,12 @@ if uploadcsv:
     editfile = st.data_editor(csvfile,use_container_width=True)
 
     save1,save2 = st.columns(2)
-    with save1:
-        if st.button('Save on Original File'):
-            savedfile = editfile.to_csv(uploadcsv.name,index=False)
-            st.success('Edited CSV File Saved')
+    #with save1:
+        #if st.button('Save on Original File'):
+            #savedfile = editfile.to_csv(uploadcsv.name,index=False)
+            #st.success('Edited CSV File Saved')
 
-    with save2:
+    with save1:
         with open(uploadcsv.name,'rb') as file: #open to make the file readable as each character
             data = file.read() #read the content
         st.download_button(label='Download Edited CSV file',data=data,file_name=uploadcsv.name)
-        
